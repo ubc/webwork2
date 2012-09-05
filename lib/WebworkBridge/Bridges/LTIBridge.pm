585e2de2fa741d415474cb33e00e6e3183cc9cbf
Compass <pan.luo@ubc.ca>
2012-09-04 22:40:11 -0700

    added detailed message for error #015.

--- lib/WebworkBridge/Bridges/LTIBridge.pm
+++ lib/WebworkBridge/Bridges/LTIBridge.pm
@@ -286,7 +286,7 @@ sub _verifyMessage()
 	my $ret = $ltiauthen->authenticate();
 	if (!$ret)
 	{
-		return error("Error: LTI message integrity could not verified.","#e015");
+		return error("Error: LTI message integrity could not verified. Check if the LTI launch URL has trailing slash.","#e015");
 	}
 	return 0;
 }
