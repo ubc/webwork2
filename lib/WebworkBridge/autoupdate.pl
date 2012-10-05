19a47920031e377d261bfbcc3358949f6a41086e
John Hsu <john.hsu@ubc.ca>
2012-10-05 12:53:58 -0700

    Enable grade submission option for auto-update.
    Pass in the -g or -grade parameter to enable LTI grade sync.
    Pass in the -c or -check parameter to only check if roster requests are
    working.
    
    Use the -h or -help option to get more information on options.

--- lib/WebworkBridge/autoupdate.pl
+++ lib/WebworkBridge/autoupdate.pl
@@ -1,5 +1,37 @@
 #!/usr/bin/env perl
 
+=head1 NAME
+
+autoupdate - Will try to update all known courses from classlist/loginlist files
+
+=head1 SYNOPSIS
+
+autoupdate [options]
+
+ Options:
+   -check	Only check to see if LTI roster requests are successful.
+   -grade	Try to send grades to the LMS.
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-check>
+
+LTI auto-updates are regular HTTP requests, there is no way
+to know if it failed since the response we get back is just the Webwork
+page. So we have this separate param to call another script that checks
+if the LTI roster requests are actually successful. 
+
+=item B<-grade>
+
+Enables additional parameters in the LTI launch request that tells
+Webwork to try sending grades to the LMS.
+
+=back
+
+=cut
+
 use strict;
 use warnings;
 
@@ -13,14 +45,26 @@ BEGIN
 
 use lib "$ENV{WEBWORK_ROOT}/lib";
 use WeBWorK::CourseEnvironment;
+use Getopt::Long;
+use Pod::Usage;
 
 # check params
-# LTI auto-updates are regular HTTP requests, there is no way
-# to know if it failed since the response we get back is just the Webwork
-# page. So we have this separate param to call another script that checks
-# if the LTI roster requests are actually successful. Right now, we're
-# not checking for any particular string, just that there is a param.
-my $check = shift;
+my $check = '';
+
+# if set to true, will try to send grades to the LMS
+my $grade = '';
+
+my $man = 0;
+my $help = 0;
+
+GetOptions (
+	"check" => \$check,
+	"grade" => \$grade,
+	'help|?' => \$help,
+	man => \$man
+); 
+
+pod2usage(1) if $help;
 
 # bring up a minimal course environment
 my $ce = WeBWorK::CourseEnvironment->new({
@@ -79,13 +123,18 @@ foreach (@lines)
 
 	my $cmd;
 
+	if ($grade)
+	{
+		$grade = "true";
+	}
+
 	if ($check)
 	{
 		$cmd = $ENV{WEBWORK_ROOT} . "/lib/WebworkBridge/checkclass_lti.pl $user $courseName $courseID $courseURL $key";
 	}
 	else
 	{
-		$cmd = $ENV{WEBWORK_ROOT} . "/lib/WebworkBridge/updateclass_lti.pl $user $courseName $courseID $courseURL $key";
+		$cmd = $ENV{WEBWORK_ROOT} . "/lib/WebworkBridge/updateclass_lti.pl $user $courseName $courseID $courseURL $key $grade";
 	}
 	my $ret = `$cmd\n`;
 	if ($?)
--- lib/WebworkBridge/updateclass_lti.pl
+++ lib/WebworkBridge/updateclass_lti.pl
@@ -24,7 +24,7 @@ BEGIN {
 	eval "use WeBWorK::CourseEnvironment"; die $@ if $@;
 }
 
-if (scalar(@ARGV) != 5) 
+if (scalar(@ARGV) < 5) 
 {
 	print "Parameter count incorrect, please enter all parameters:";
 	print "\nupdateclass UserID CourseName CourseID CourseURL Key\n";
@@ -33,6 +33,7 @@ if (scalar(@ARGV) != 5)
 	print "\nCourseID - The course's ID in the site we're updating from.";
 	print "\nCourseURL - The url we submit LTI membership requests to.";
 	print "\nKey - The consumer key to use for calculation LTI OAuth hash.\n";
+	print "\nGrades (Optional) - If given, will try to send grades to LMS.\n";
 	print "\ne.g.: updateclass A1B2C3D4 Math100-100 _100_1 https://lms.ubc.ca/lti/membership LTISecret\n";
 	exit();
 }
@@ -42,6 +43,7 @@ my $courseName = shift;
 my $courseID = shift;
 my $courseURL = shift;
 my $key = shift;
+my $grade = shift;
 
 # bring up a minimal course environment
 my $ce = WeBWorK::CourseEnvironment->new({
@@ -59,6 +61,13 @@ my $request_url = $ce->{server_root_url} . $ce->{webwork_url};
 if (substr($request_url, -1, 1) ne "/")
 {
 	$request_url .= "/";
+}
+
+my %gradeParams;
+if (defined($grade))
+{
+	$gradeParams{'ext_ims_lis_basic_outcome_url'} = $courseURL;
+	$gradeParams{'ext_ims_lis_resultvalue_sourcedids'} = "ratio";
 }
 
 my $request = Net::OAuth->request("request token")->new(
@@ -85,7 +94,8 @@ my $request = Net::OAuth->request("request token")->new(
 		ext_ims_lis_memberships_id => $courseID,# store
 		ext_ims_lis_memberships_url => $courseURL, # store
         ubc_auto_update => 'true',
-        roles => 'AutoUpdater'
+        roles => 'AutoUpdater',
+		%gradeParams
 	}
 );
 $request->sign;
