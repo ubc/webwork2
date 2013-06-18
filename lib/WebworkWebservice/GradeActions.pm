d31f44552c4a17bef71dac9f58bb8e55814cea10
John Hsu <john.hsu@ubc.ca>
2013-06-18 16:23:35 -0700

    Fix grade api not working with gateway/quizzes.
    Either I removed something critical or there was some database changes
    that rendered my original code from LTI grades to fail. Will have to
    check LTI grade import later.
    
    The API was returning 0s for all grade related fields. Comparison of
    code with ContentGenerator/Instructor/StudentProgress.pm showed some
    changes so I've brought those changes over.
    
    There are two types of grades, versioned and non-versioned. Versioned
    grades are from gateway/quizzes assignments. Regular homeworks are
    non-versioned. Versioned just means that students can attempt multiple
    versions of that set (e.g.: attempting the same quiz multiple times
    will result in questions with different numbers).
    
    The grade hash structure:
    PG File Path ->
      status - Has the user answered this question correctly?
      numIncorrect - Number of times the user answered incorrectly.
      numCorrect - Number of times the user answered correctly.
      attempted - 1 if the user has attempted this question 0 otherwise
    Total ->
      version - this field exists if we're checking a versioned quiz where
      a user
      	can attempt a quiz multiple times.
      numAttempts - Total number of attempts for every problem.
      totalRight - Number of questions that the user has gotten completely
      right.
      status - How many questions the user got right. Usually, each
      question
      	counts for 1 point. If a question has multiple parts, then each
      	part
      	that the user got right would count as a fraction of the total 1
      	point.
      score - Score out of 100 that the user got for this assignment
      total - total number of points possible

--- lib/WebworkWebservice/GradeActions.pm
+++ lib/WebworkWebservice/GradeActions.pm
@@ -11,6 +11,7 @@ use WeBWorK::DB;
 use WeBWorK::Debug;
 use WeBWorK::CourseEnvironment;
 use WeBWorK::Localize;
+use WeBWorK::Utils::Grades qw/list_set_versions/;
 
 use Data::Dumper;
 use JSON;
@@ -32,10 +33,10 @@ use MIME::Base64 qw( encode_base64 decode_base64);
 # so, get the grades, if not, skip
 sub getDetailedSetGrades
 {
-    my ($self, $params) = @_;
-    my $out = {};
-    my $db = $self->{db};
-    my $ce = $self->{ce};
+	my ($self, $params) = @_;
+	my $out = {};
+	my $db = $self->{db};
+	my $ce = $self->{ce};
 
 	my $setid = $params->{'set_id'};
 
@@ -48,15 +49,19 @@ sub getDetailedSetGrades
 	my %scores = ();
 	foreach my $userid (@userIDs)
 	{ # grab the grades for each user
+		my $studentRecord = $db->getUser($userid) 
+			or die "record for user $userid not found";
+		# skip if we're not supposed to include this user in states
+		next unless $ce->status_abbrev_has_behavior($studentRecord->status, "include_in_stats");
 		# get this user's version of the set
 		my $set = $db->getMergedSet($userid, $setid);
 		# retrieve grades using this user's set version
 		my $setName = $set->set_id;
-		$scores{$userid} = getGradeRecords($self, $set, $db, $userid);
+		$scores{$userid} = getGrades($self, $db, $set, $userid);
 	}
 
 	$out->{'grades'} = \%scores;
-	debug("Grades done: ". Dumper($out));
+	debug("Grades done.");
 
 	return $out;
 }
@@ -67,55 +72,116 @@ sub getDetailedSetGrades
 # There are 2 types of assignments with different grade types,
 # the gateway quizzes may have multiple scores for multiple tries,
 # hence they're returned as an array of score records.
-sub getGradeRecords
+# 
+# Non-versioned sets returns a hash.
+# Versioned sets will return an array of hashes.
+# 
+# Versioned sets comes from quizzes where multiple attempts are allowed
+# 
+# Both version will return the same hash structure:
+# PG File Path ->
+#	status - Has the user answered this question correctly? 
+#	numIncorrect - Number of times the user answered incorrectly.
+#	numCorrect - Number of times the user answered correctly.
+#	attempted - 1 if the user has attempted this question 0 otherwise
+# Total ->
+#	version - this field exists if we're checking a versioned quiz where a user
+#		can attempt a quiz multiple times.
+#	numAttempts - Total number of attempts for every problem.
+#	totalRight - Number of questions that the user has gotten completely right.
+#	status - How many questions the user got right. Usually, each question
+#		counts for 1 point. If a question has multiple parts, then each part
+#		that the user got right would count as a fraction of the total 1 point.
+#	score - Score out of 100 that the user got for this assignment
+#	total - total number of points possible
+# 
+sub getGrades
 {
-	my ($self, $set, $db, $userid) = @_;
-	my $setName = $set->set_id;
+	my ($self, $db, $set, $userid) = @_;
 
+	my $isVersioned = 0;
 	if (defined($set->assignment_type) && 
 		$set->assignment_type =~ /gateway/) 
 	{ # this set allows multiple attempts and can record many scores 
-		# get all attempts
-		my @vList = $db->listSetVersions($userid,$setName);
-		my @setVersions = $db->getMergedSetVersions( 
-			map {[$userid, $setName, $_]} @vList );
+		$isVersioned = 1;
+	}
 
-		# calculate and store score for each attempt
-		my @scores =
-			map {getGradeRecord($self, $db, $set, $userid, 1)} @setVersions;
-		return \@scores;
+	# Grab set version numbers
+	my( $ra_allSetVersionNames, $notAssignedSet) = list_set_versions($db, $userid, $set->set_id, $isVersioned);
+	my @allSetVersionNames = @{$ra_allSetVersionNames};
+
+	my $ret;
+	if ($isVersioned) {
+		$ret = ();
 	}
-	else 
-	{ # only one score will be recorded for this set
-		return getGradeRecord($self, $db, $set, $userid, 0);
+	else {
+		$ret = {};
 	}
+
+	foreach my $setName ( @allSetVersionNames ) {
+
+		my $status          = 0;
+		my $longStatus      = '';
+		my $string          = '';
+		my $twoString       = '';
+		my $totalRight      = 0;
+		my $total           = 0;
+		my $total_num_of_attempts_for_set = 0;
+		my %h_problemData   = ();
+		my $num_of_attempts;
+		my $num_of_problems;
+
+		my $set;
+		my $userSet;
+		my $version;
+		if ( $isVersioned ) {
+			my ($setN,$vNum) = ($setName =~ /(.+),v(\d+)$/);
+			$version = $vNum;
+			# we'll also need information from the set
+			#    as we set up the display below, so get
+			#    the merged userset as well
+			$set = $db->getMergedSetVersion($userid, $setN, $vNum);
+			$userSet = $set;
+			$setName = $setN;
+		} else {
+			$set = $db->getMergedSet( $userid, $setName );
+
+		}
+
+		# Create an empty set if we didn't get one? Not sure why this is here.
+		unless ( ref($set) ) {
+			$set = new WeBWorK::DB::Record::UserSet;
+			$set->set_id($setName);
+		}
+
+		my $grades = grade_set( $db, $set, $setName, $userid, $isVersioned);
+		if ($isVersioned) {
+			$grades->{'total'}{'version'} = $version;
+			push(@{$ret}, $grades);
+		}
+		else {
+			$ret = $grades;
+		}
+	}
+
+	return $ret;
 }
 
-# Return a hash of grade attributes for user's set. The hash
-# is composed of 3 elements:
-# status - whether the user has attempted this assignment yet
-# totalRight - how many points the user got for answering correctly
-# total - total number of points possible
-sub getGradeRecord
-{
-	my ($self, $db, $set, $userid, $isVersioned) = @_;
-	return grade_set($self, $db, $set, $userid, $isVersioned);
-}
-
+# grab grades for a single set
 sub grade_set {
-	my ($self, $db, $set, $studentName, $setIsVersioned) = @_;
 
-	my $setID = $set->set_id;
+	my ($db, $set, $setName, $studentName, $setIsVersioned) = @_;
+
+	my $setID = $set->set_id();  #FIXME   setName and setID should be the same
 
 	my $status = 0;
 	my $longStatus = '';
 	my $string     = '';
-	my $twoString  = '';
 	my $totalRight = 0;
 	my $total      = 0;
 	my $num_of_attempts = 0;
 
-	debug("Begin collecting problems for set $setID");
+	debug("Collecting problems for user $studentName and set $setName");
 	# DBFIXME: to collect the problem records, we have to know 
 	#    which merge routines to call.  Should this really be an 
 	#    issue here?  That is, shouldn't the database deal with 
@@ -124,46 +190,15 @@ sub grade_set {
 
 	my @problemRecords = $db->getAllMergedUserProblems( $studentName, $setID );
 	my $num_of_problems  = @problemRecords || 0;
-	my $max_problems     = defined($num_of_problems) ? $num_of_problems : 0; 
-
 	if ( $setIsVersioned ) {
-		@problemRecords = $db->getAllMergedProblemVersions( $studentName, $setID, $set->version_id );
-	}   # use versioned problems instead (assume that each version has the same number of problems.
-
-	debug("End collecting problems for set $setID");
-
-	####################
-	# Resort records
-	#####################
-	@problemRecords = sort {$a->problem_id <=> $b->problem_id }  @problemRecords;
-
-	# for gateway/quiz assignments we have to be careful about 
-	#    the order in which the problems are displayed, because
-	#    they may be in a random order
-	if ( $set->problem_randorder ) {
-		my @newOrder = ();
-		my @probOrder = (0..$#problemRecords);
-		# we reorder using a pgrand based on the set psvn
-		my $pgrand = PGrandom->new();
-		$pgrand->srand( $set->psvn );
-		while ( @probOrder ) { 
-			my $i = int($pgrand->rand(scalar(@probOrder)));
-			push( @newOrder, $probOrder[$i] );
-			splice(@probOrder, $i, 1);
-		}
-		# now $newOrder[i] = pNum-1, where pNum is the problem
-		#    number to display in the ith position on the test
-		#    for sorting, invert this mapping:
-		my %pSort = map {($newOrder[$_]+1)=>$_} (0..$#newOrder);
-
-		@problemRecords = sort {$pSort{$a->problem_id} <=> $pSort{$b->problem_id}} @problemRecords;
+		@problemRecords =  $db->getAllMergedProblemVersions( $studentName, $setID, $set->version_id );
 	}
-
 
 	#######################################################
 	# construct header
 
 	my $ret = {};
+
 	foreach my $problemRecord (@problemRecords) {
 		my $prob = $problemRecord->problem_id;
 
@@ -175,11 +210,12 @@ sub grade_set {
 
 		my $pgfile = $problemRecord->source_file;
 		debug("Processing problem: $prob | $pgfile");
+
 		$status           = $problemRecord->status || 0;
-		my  $attempted    = $problemRecord->attempted;
+		my $attempted     = $problemRecord->attempted;
 		my $num_correct   = $problemRecord->num_correct || 0;
 		my $num_incorrect = $problemRecord->num_incorrect   || 0;
-		$num_of_attempts  += $num_correct + $num_incorrect;
+		$num_of_attempts  = $num_correct + $num_incorrect;
 
 		$ret->{$pgfile}{'status'} = $status;
 		$ret->{$pgfile}{'attempted'} = $attempted;
@@ -191,7 +227,7 @@ sub grade_set {
 		# the problem is marked as attempted if the status has
 		# been set or if the problem has been attempted
 		# DBFIXME this should happen in the database layer, not here!
-		if (!$attempted && ($status || $num_correct || $num_incorrect )) {
+		if (!$attempted && ($status || $num_of_attempts)) {
 			$attempted = 1;
 			$problemRecord->attempted('1');
 			# DBFIXME: this is another case where it 
@@ -212,48 +248,34 @@ sub grade_set {
 		###########################################
 		# Determine the string $longStatus which 
 		# will display the student's current score
-		###########################################			
+		###########################################
 
 		if (!$attempted){
-			$longStatus     = '.';
+			$longStatus     = 0;
 		} elsif   ($valid_status) {
-			$longStatus     = int(100*$status+.5);
-			$longStatus='C' if ($longStatus==100);
+			$longStatus     =  int(100*$status+.5) ;
 		} else	{
-			$longStatus 	= 'X';
+			$longStatus 	= '-';
 		}
 
-		$string          .= threeSpaceFill($longStatus);
-		$twoString       .= threeSpaceFill($num_incorrect);
-		my $probValue     = $problemRecord->value;
-		$probValue        = 1 unless defined($probValue) and $probValue ne "";  # FIXME?? set defaults here?
-		$total           += $probValue;
-		$totalRight      += round_score($status*$probValue) if $valid_status;
-
-
-	}  # end of problem record loop
-
+		my $probValue   =  $problemRecord->value;
+		$probValue      =  1 unless defined($probValue) and $probValue ne "";  # FIXME?? set defaults here?
+		$total          += $probValue;
+		$totalRight     += round_score($status*$probValue) if $valid_status;
+	}
 
 	$ret->{'total'} = {
 		status => $status,
 		totalRight => $totalRight,
 		total => $total,
-		numAttempts => $num_of_attempts
+		numAttempts => $num_of_attempts,
+		score => $longStatus
 	};
 
 	return $ret;
 }
 
-
 # Utility functions from ContentGenerator/Grades.pm
-sub threeSpaceFill {
-	my $num = shift @_ || 0;
-
-	if (length($num)<=1) {return "$num".'&nbsp;&nbsp;';}
-	elsif (length($num)==2) {return "$num".'&nbsp;';}
-	else {return "## ";}
-}
-
 sub round_score {
 	return shift;
 }
