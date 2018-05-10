ubc/webwork2

commit 8ced0c78d2a386af977f43e00ade7323d48e3249

Fixed a bug where using the 'Force Renumber' option with reordering undid reordering.

htdocs/js/apps/ProblemSetDetail2/problemsetdetail2.js
@@ -1,8 +1,13 @@
 $(function() {
 
+
     //Problem set detail 2 
     $('#problemset_detail_list').addClass('container-fluid');
 
+    //This sets ajax coos to be synchronous so as to not overwhelm the server
+    $.ajax({async : false});
+
+    
     //This uses the nextedSortable jquery-ui module to drive the 
     // problem list, if its enabled
 

lib/WeBWorK/ContentGenerator/Instructor/ProblemSetDetail2.pm
@@ -1714,7 +1714,6 @@ sub initialize {
 		##################################################################
 
 		my %newProblemNumbers = ();
-		my $consecutive = $r->param('force_renumber') || 0;
 		my $prevNum = 0;
 		my @prevSeq = (0);
 
@@ -1733,36 +1732,63 @@ sub initialize {
 			    unshift @idSeq, $r->param('prob_num_'.$id);
 			}
 
-			if ($consecutive) {
-			    # we dont really care about the content of idSeq
-			    # in this case, just the length
-			    my $depth = $#idSeq;
-			    
-			    if ($depth <= $#prevSeq) {
-				@prevSeq = @prevSeq[ 0 .. $depth ];
-				$prevSeq[$#prevSeq]++;
-			    } else {
-				$prevSeq[$#prevSeq+1] = 1;
-			    }
-			    
-			    $newProblemNumbers{$jj} = seq_to_jitar_id(@prevSeq);
-			    
-			} else {
-			    $newProblemNumbers{$jj} = seq_to_jitar_id(@idSeq);
-			}
-			
+			$newProblemNumbers{$jj} = seq_to_jitar_id(@idSeq);
+						
 		    } else {
-			if ($consecutive) {
-			    $prevNum++;
-			    $newProblemNumbers{$jj} = $prevNum;
-			} else {
-			    $newProblemNumbers{$jj} = $r->param('prob_num_' . $jj);
-			} 
+		      $newProblemNumbers{$jj} = $r->param('prob_num_' . $jj);
 		    }
-		}
+		  }
 		
 		handle_problem_numbers($self,\%newProblemNumbers, $db, $setID) unless defined $r->param('undo_changes');
+
 		
+		#####################################################################
+		# Make problem numbers consecutive if required
+		#####################################################################
+
+
+		if ($r->param('force_renumber')) {
+
+		  my %newProblemNumbers = ();
+		  my $prevNum = 0;
+		  my @prevSeq = (0);
+		
+		  for my $jj (sort { $a <=> $b } $db->listGlobalProblems($setID)) {
+		    
+		    if ($setRecord->assignment_type eq 'jitar') {
+		      my @idSeq;
+		      my $id = $jj;
+		      
+		      next unless $r->param('prob_num_'.$id);
+		      
+		      unshift @idSeq, $r->param('prob_num_'.$id);
+		      while (defined $r->param('prob_parent_id_'.$id)) {
+			$id = $r->param('prob_parent_id_'.$id);
+			unshift @idSeq, $r->param('prob_num_'.$id);
+		      }
+		      
+		      # we dont really care about the content of idSeq
+		      # in this case, just the length
+		      my $depth = $#idSeq;
+		      
+		      if ($depth <= $#prevSeq) {
+			@prevSeq = @prevSeq[ 0 .. $depth ];
+			$prevSeq[$#prevSeq]++;
+		      } else {
+			$prevSeq[$#prevSeq+1] = 1;
+		      }
+		      
+		      $newProblemNumbers{$jj} = seq_to_jitar_id(@prevSeq);
+						
+		    } else {
+		      $prevNum++;
+		      $newProblemNumbers{$jj} = $prevNum;
+		    }
+		  }
+		
+		  handle_problem_numbers($self,\%newProblemNumbers, $db, $setID) unless defined $r->param('undo_changes');
+
+		}
 
 		#####################################################################
 		# Add blank problem if needed

commit d1c6e1c3a267105a82ff2c1777df2be16298c5fa

Fix session key reset on problem set edit screen
Introduced in 8ced0c78d the frontend would send a ajax request on screen load (no url provided so would use current url).
- On first page load there wouldn't be a problem since the user and session key are in the query string
- saving the form causes a post so user and session key are no longer in the query string when page reloads
- ajax request causes the authentication to fire again generating a new session key that the current browser window is not aware of (hence the next action is invalid)

Fixed by commenting out the ajax request since I don't think it does anything.
I believe the original intention was to make ajax requests on the page synchronous so not to overload the server but this doesn't actually do that (they might have meant to use `ajaxSetup()` which would effect future requests).

htdocs/js/apps/ProblemSetDetail2/problemsetdetail2.js
@@ -5,7 +5,7 @@ $(function() {
     $('#problemset_detail_list').addClass('container-fluid');
 
     //This sets ajax coos to be synchronous so as to not overwhelm the server
-    $.ajax({async : false});
+    //$.ajax({async : false});
 
     
     //This uses the nextedSortable jquery-ui module to drive the 